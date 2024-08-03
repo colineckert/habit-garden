20cc3b8c2b71b1bbb05f0b09a42fbf9be523dcdd
Colin Eckert <colineckert10@gmail.com>
2024-08-02 22:50:55 -0700

    update type import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import type { Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { TopNav } from "./_components/topnav";
