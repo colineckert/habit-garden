a038df8c95f6c5186bb5b2804e522ce608317032
Colin Eckert <colineckert10@gmail.com>
2024-08-03 08:05:07 -0700

    get habits by user

--- src/server/api/routers/habit.ts
+++ src/server/api/routers/habit.ts
@@ -1,22 +1,38 @@
-import { z } from "zod";
+import { auth } from "@clerk/nextjs/server";
+// import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { habits } from "~/server/db/schema";
+// import { habits } from "~/server/db/schema";
 
 export const habitRouter = createTRPCRouter({
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(habits).values({
-        name: input.name,
-      });
-    }),
+  getAllByUser: publicProcedure.query(async ({ ctx }) => {
+    const user = auth();
+
+    if (!user.userId) throw new Error("Unauthorized");
+
+    const userHabits = ctx.db.query.habits.findMany({
+      where: (habits, { eq }) => eq(habits.userId, user.userId),
+      orderBy: (habits, { desc }) => desc(habits.createdAt),
+    });
+
+    return userHabits;
+  }),
+
+  // create: publicProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     const user = auth();
+  //     console.log("api*****", user);
+  //     await ctx.db.insert(habits).values({
+  //       name: input.name,
+  //       userId: user.userId!,
+  //     });
+  //   }),
 
   // getLatest: publicProcedure.query(async ({ ctx }) => {
   //   const post = await ctx.db.query.posts.findFirst({
   //     orderBy: (posts, { desc }) => [desc(posts.createdAt)],
   //   });
-
   //   return post ?? null;
   // }),
 });
