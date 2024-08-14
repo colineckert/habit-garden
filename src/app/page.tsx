8569d2896cb98238e896d35bd6a13ad08da69349
Colin Eckert <colineckert10@gmail.com>
2024-08-14 07:16:04 -0700

    connect db to basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { db } from "~/server/db";
+
 export const dynamic = "force-dynamic";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const habits = await db.query.habits.findMany();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
@@ -8,6 +12,13 @@ export default function HomePage() {
           Welcome to Habit Garden
         </h1>
       </div>
+      <ul>
+        {habits.map((habit) => (
+          <li key={habit.id}>
+            <h2>{habit.name}</h2>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
