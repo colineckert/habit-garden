bc6439bca24749c987981e98ff15f226fc844682
Colin Eckert <colineckert10@gmail.com>
2024-08-13 21:23:42 -0700

    setup db and update schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -22,7 +22,8 @@ export const habits = createTable(
   "habit",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    userId: varchar("userId", { length: 256 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
