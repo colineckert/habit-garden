8a6ff555df1e14066a1a46d64f1211880040408a
Colin Eckert <colineckert10@gmail.com>
2024-08-15 07:52:22 -0700

    move _components dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-import { TopNav } from "~/_components/TopNav";
+import { TopNav } from "./_components/TopNav";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
