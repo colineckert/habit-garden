bea30e6e7d5f213a6fc9c5e9b2587d0269b10060
Colin Eckert <colineckert10@gmail.com>
2024-08-19 07:52:39 -0700

    habit create and delete queries

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,5 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "./db";
+import { habits } from "./db/schema";
+import { and, eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export async function getUserHabits() {
   const user = auth();
@@ -12,4 +15,29 @@ export async function getUserHabits() {
   });
 
   return habits;
+}
+
+export async function createHabit(name: string) {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db.insert(habits).values({
+    name,
+    userId: user.userId,
+  });
+
+  redirect("/");
+}
+
+export async function deleteHabit(id: number) {
+  const user = auth();
+
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(habits)
+    .where(and(eq(habits.id, id), eq(habits.userId, user.userId)));
+
+  redirect("/");
 }
